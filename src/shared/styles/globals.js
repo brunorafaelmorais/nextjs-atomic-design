43bbde60153219e0ccea974f4028b7cbe135dca9
bruno.morais <bruno.morais@nodo.cc>
2021-04-13 08:16:11 -0300

    feat(styles): add normalize

--- src/shared/styles/globals.js
+++ src/shared/styles/globals.js
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'polished';
 
 const GlobalStyle = createGlobalStyle`
+  ${normalize()};
+
   *, *::before, *::after {
     box-sizing: border-box;
   }
